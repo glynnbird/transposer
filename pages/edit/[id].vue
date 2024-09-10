39924d0201efb7a857619bfaa22bea000cd41bc1
Glynn Bird <glynn.bird@gmail.com>
2024-09-10 09:28:01 +0100

    delete

--- pages/edit/[id].vue
+++ pages/edit/[id].vue
@@ -9,6 +9,11 @@
     await db.put(song.value)
     await navigateTo(`/song/${song.value._id}`)
   }
+
+  const deleteSong = async () => {
+    await db.remove(song.value._id, song.value._rev)
+    await navigateTo('/')
+  }
   
 </script>
 <style setup>
@@ -24,4 +29,7 @@ textarea {
   <v-text-field label="Artist" v-model="song.artist"></v-text-field>
   <v-textarea label="Tab" v-model="song.tab"></v-textarea>
   <v-btn :disabled="!song.artist || !song.song" color="success" @click="save">Edit Tab</v-btn>
+  <br /><br />
+
+  <v-btn color="error" @click="deleteSong">Delete</v-btn>
 </template>
