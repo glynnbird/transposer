b95ccb405e906ee20077fa28111495c0d8f7527c
Glynn Bird <glynn.bird@gmail.com>
2024-09-10 10:31:07 +0100

    only shuffle from the home page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <script setup>
   // state
+  const route = useRoute()
   const shuffleList = useShuffleList()
   let pick = 0
 
@@ -28,7 +29,7 @@
         <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       </template>
       <v-app-bar-title @click="clickHome()">Transposer</v-app-bar-title>
-      <template v-slot:append>
+      <template v-slot:append v-if="route.name === 'index'">
         <v-btn icon="mdi-shuffle" @click="shuffle"></v-btn>
       </template>
     </v-app-bar>
