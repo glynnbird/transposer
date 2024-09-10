9ea45c9416171e999e27cbc2949e507e40b8c0d9
Glynn Bird <glynn.bird@gmail.com>
2024-09-10 09:16:29 +0100

    add and edit

--- pages/add.vue
+++ pages/add.vue
@@ -1,9 +1,29 @@
 <script setup>
- 
+  const db = new PouchDB('transposer')
+  const song = ref(0)
+  song.value = {
+    song: '',
+    artist: '',
+    tab: ''
+  }
+
+  const save = async () => {
+    const response = await db.post(song.value)
+    await navigateTo(`/song/${response.id}`)
+  }
+  
 </script>
 <style setup>
-
+textarea {
+  font-family: Consolas, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace !important;
+  font-size: 12px !important;
+  height: 500px
+}
 </style>
 <template>
-  <h2>Add</h2>
+  <h3>Edit</h3>
+  <v-text-field label="Song name" v-model="song.song"></v-text-field>
+  <v-text-field label="Artist" v-model="song.artist"></v-text-field>
+  <v-textarea label="Tab" v-model="song.tab"></v-textarea>
+  <v-btn :disabled="!song.artist || !song.song" color="success" @click="save">Save</v-btn>
 </template>
--- pages/edit/[id].vue
+++ pages/edit/[id].vue
@@ -4,9 +4,24 @@
   const song = ref(0)
   const id = route.params.id
   song.value = await db.get(id)
+
+  const save = async () => {
+    await db.put(song.value)
+    await navigateTo(`/song/${song.value._id}`)
+  }
   
 </script>
+<style setup>
+textarea {
+  font-family: Consolas, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace !important;
+  font-size: 12px !important;
+  height: 500px
+}
+</style>
 <template>
-  <h3>Edit {{ song.song }} - {{ song.artist }}</h3>
-  
+  <h3>Edit</h3>
+  <v-text-field label="Song name" v-model="song.song"></v-text-field>
+  <v-text-field label="Artist" v-model="song.artist"></v-text-field>
+  <v-textarea label="Tab" v-model="song.tab"></v-textarea>
+  <v-btn :disabled="!song.artist || !song.song" color="success" @click="save">Edit Tab</v-btn>
 </template>
