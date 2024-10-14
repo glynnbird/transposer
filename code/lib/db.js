797ac3d2d20720bbc49fc0401db0b91aaaf88cec
Glynn Bird <glynn.bird@gmail.com>
2024-10-14 14:25:36 +0100

    simplify

--- code/lib/db.js
+++ code/lib/db.js
@@ -1,22 +1,5 @@
-import { porterStemmer } from './stemmer.js'
-import { process } from './process.js'
-
-export const toggle = async function(kv, id) {
-  const { value, metadata } = await kv.getWithMetadata(`doc:${id}`)
-  if (value === null) {
-    return { ok: false }
-  }
-  const j = JSON.parse(value)
-  await del(kv, id)
-  j.doc.watching = !j.doc.watching
-  j.metadata = metadata
-  j.metadata.watching = j.doc.watching
-  await add(kv, j)
-  return { ok: true}
-}
-
 export const get = async function(kv, id) {
-  const r = await kv.get(`doc:${id}`)
+  const r = await kv.get(is=d)
   if (r === null) {
     return { ok: false }
   } else {
@@ -37,17 +20,6 @@ export const list = async function(kv) {
   return output
 }
 
-export const queryIndex = async function(kv, key, value) {
-  const l = await kv.list({ prefix: `index:${key}:${value}` })
-  const output = l.keys.map((k) => {
-    return {
-      id: k.name.replace(/^.+:/,''),
-      ...k.metadata
-    }
-  })
-  return output
-}
-
 export const add = async function (kv, json) {
   if (!json.id) {
     json.id = new Date().getTime().toString()
@@ -55,42 +27,10 @@ export const add = async function (kv, json) {
   if (!json.metadata) {
     json.metadata = {}
   }
-  if (!json.freetext) {
-    json.freetext = ''
-  }
 
   // if there's all the parts we need
   if (json.id && json.doc && json.metadata) {
-    const words = process(json.freetext).map((w) => { return porterStemmer(w) })
-
-    // write core doc
-    const coreDoc = {
-      id: json.id,
-      doc: json.doc,
-      _ts: new Date().toISOString(),
-      _freetext: json.freetext,
-      _freetextIndex: words,
-      _index: json.index
-    }
-    delete coreDoc.doc.metadata
-    const m = {
-      metadata: json.metadata
-    }
-    await kv.put(`doc:${json.id}`, JSON.stringify(coreDoc), m)
-
-    // // write secondary index docs for freetext search
-    // for (const word of words) {
-    //   await kv.put(`freetext:${word}:${json.id}`, null, { metadata: json.metadata })
-    // }
-
-    // // write secondary index docs for indexed items
-    // if (json.index) {
-    //   const keys = Object.keys(json.index)
-    //   for (const key of keys) {
-    //     const v = json.index[key]
-    //     await kv.put(`index:${key}:${v}:${json.id}`, null, { metadata: json.metadata })
-    //   }
-    // }
+    await kv.put(json.id, JSON.stringify(json.doc), { metadata: json.metadata })
 
     // send response
     return { ok: true, id: json.id }
@@ -101,30 +41,9 @@ export const add = async function (kv, json) {
 }
 
 export const del = async function (kv, id) {
-  // const r = await kv.get(`doc:${id}`)
-  // const json = JSON.parse(r)
-  // if (!json) {
-  //   return { ok: false }
-  // }
-
-  // // delete free-text index
-  // if (json['_freetextIndex']) {
-  //   for (const word of json['_freetextIndex']) {
-  //     await kv.delete(`freetext:${word}:${id}`)
-  //   }
-  // }
-
-  // // write secondary index docs for indexed items
-  // if (json['_index']) {
-  //   const keys = Object.keys(json['_index'])
-  //   for (const key of keys) {
-  //     const v = json._index[key]
-  //     await kv.delete(`index:${key}:${v}:${id}`)
-  //   }
-  // }
 
   // delete original doc
-  await kv.delete(`doc:${id}`)
+  await kv.delete(id)
 
   return { ok: true }
 }
