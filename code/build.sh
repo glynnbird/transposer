5cc4ae45f74d5e79440a90e75daf4d81f865b17e
Glynn Bird <glynn.bird@gmail.com>
2024-10-14 14:34:54 +0100

    remove terser

--- code/build.sh
+++ code/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 npx rollup --format=es --file=../functions/api/add.js -- add.js
 #npx rollup -p @rollup/plugin-terser --format=es --file=../functions/api/del.js -- del.js
-npx rollup -p @rollup/plugin-terser --format=es --file=../functions/api/get.js -- get.js
+npx rollup --format=es --file=../functions/api/get.js -- get.js
 npx rollup -p @rollup/plugin-terser --format=es --file=../functions/api/list.js -- list.js
 #npx rollup -p @rollup/plugin-terser --format=es --file=../functions/api/query.js -- query.js
 #npx rollup -p @rollup/plugin-terser --format=es --file=../functions/api/toggle.js -- toggle.js
