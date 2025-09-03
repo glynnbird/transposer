80412cea39ac487b56977f61197ad8b92834ba81
Glynn Bird <glynn.bird@gmail.com>
2025-09-03 13:19:58 +0100

    rolldown and nuxt@4.1.0

--- code/build.sh
+++ code/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-npx rollup -p @rollup/plugin-terser --format=es --file=../functions/api/add.js -- add.js
-npx rollup -p @rollup/plugin-terser --format=es --file=../functions/api/del.js -- del.js
-npx rollup -p @rollup/plugin-terser --format=es --file=../functions/api/get.js -- get.js
-npx rollup -p @rollup/plugin-terser --format=es --file=../functions/api/list.js -- list.js
+npx rolldown --format=es --file=../functions/api/add.js -- add.js
+npx rolldown --format=es --file=../functions/api/del.js -- del.js
+npx rolldown --format=es --file=../functions/api/get.js -- get.js
+npx rolldown --format=es --file=../functions/api/list.js -- list.js
 
